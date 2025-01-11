72117e6c9a5231b04c388ba4dc57179bdb6b16f7
Rizwan Ahamad <rizwanahamadcodes@gmail.com>
2025-01-11 21:15:19 +0545

    overflow hidden on aside

--- src/components/Aside.tsx
+++ src/components/Aside.tsx
@@ -21,7 +21,7 @@ const Aside = (props: AsideProps) => {
     return (
         <div
             className={clsx(
-                "transition-all",
+                "transition-all overflow-hidden",
                 sidebarCollapsed ? "w-0" : "w-3.5",
                 className
             )}>
